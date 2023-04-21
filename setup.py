41d02afcf70c29bbd79358f23d903a3b71530fc0
Michael S. P. Kelley <msk@astro.umd.edu>
2023-04-21 10:08:20 -0400

    Use FC_GFORTRAN if possible.

--- setup.py
+++ setup.py
@@ -5,100 +5,107 @@ import os
 from glob import glob
 import shutil
 import setuptools
-from distutils.spawn import find_executable
 from numpy.distutils.core import setup, Extension as NumpyExtension
 from numpy.distutils.command.build_src import build_src
 
 import subprocess
 
-def gfortran_exec():
-    """Find a gfortran executable."""
-    for name in ("gfortran", "gfortran-11", "gfortran-12", "gfortran-10"):
-        path = find_executable(name)
-        if path is not None:
-            return path
-    raise FileNotFoundError("Cannot find gfortran executable.")
 
 class PyoorbBuildSrc(build_src):
     def run(self):
+        self.env = os.environ.copy()
+        self.find_gfortran()
         self.configure()
         self.make()
         # self.download_ephem()
         self.copy_files()
         super().run()
 
+    def find_gfortran(self):
+        """Find a gfortran executable."""
+        if os.environ.get("FC_GFORTRAN") is not None:
+            gfortran = shutil.which(name)
+        else:
+            for name in ("gfortran", "gfortran-11", "gfortran-12", "gfortran-10"):
+                gfortran = shutil.which(name)
+                if gfortran is not None:
+                    break
+
+        if gfortran is None:
+            fc = (
+                ""
+                if os.environ.get("FC_GFORTRAN") is None
+                else f"  FC_GFORTRAN = {os.environ.get('FC_GFORTRAN')}"
+            )
+            raise FileNotFoundError("Cannot find gfortran executable." + fc)
+
+        self.env["FC_GFORTRAN"] = gfortran
+
     def configure(self):
         """Configures oorb to build with gfortran and optimization."""
-        
-        if not os.path.exists('oorb/Makefile.include'):
+        if not os.path.exists("oorb/Makefile.include"):
             cmd = [
-                './configure',
-                gfortran_exec(),
-                'opt',
+                "./configure",
+                "gfortran" "opt",
             ]
-            subprocess.check_call(cmd, cwd='oorb')
+            subprocess.check_call(cmd, cwd="oorb", env=self.env)
 
     def make(self):
         """Builds oorb, required to build the pyoorb extension."""
-        if not os.path.exists('oorb/lib/liboorb.a'):
-            cmd = ['make', '-j']
-            subprocess.check_call(cmd, cwd='oorb')
+        if not os.path.exists("oorb/lib/liboorb.a"):
+            cmd = ["make", "-j"]
+            subprocess.check_call(cmd, cwd="oorb", env=self.env)
 
     def download_ephem(self):
         """Downloads the default oorb ephemeris."""
-        if not os.path.exists('oorb/data/de430.dat'):
-            cmd = ['make', 'ephem']
-            subprocess.check_call(cmd, cwd='oorb')
+        if not os.path.exists("oorb/data/de430.dat"):
+            cmd = ["make", "ephem"]
+            subprocess.check_call(cmd, cwd="oorb", env=self.env)
 
     def copy_files(self):
         """Copies fortran-python interface and data files."""
-        if not os.path.exists('src'):
-            os.mkdir('src')
+        if not os.path.exists("src"):
+            os.mkdir("src")
         files = [
-            'oorb/build/planetary_data.mod',
-            'oorb/build/base_cl.mod',
-            'oorb/build/time_cl.mod',
-            'oorb/build/sphericalcoordinates_cl.mod',
-            'oorb/build/observatories_cl.mod',
-            'oorb/build/orbit_cl.mod',
-            'oorb/build/stochasticorbit_cl.mod',
-            'oorb/build/physicalparameters_cl.mod',
-            'oorb/python/pyoorb.f90',
+            "oorb/build/planetary_data.mod",
+            "oorb/build/base_cl.mod",
+            "oorb/build/time_cl.mod",
+            "oorb/build/sphericalcoordinates_cl.mod",
+            "oorb/build/observatories_cl.mod",
+            "oorb/build/orbit_cl.mod",
+            "oorb/build/stochasticorbit_cl.mod",
+            "oorb/build/physicalparameters_cl.mod",
+            "oorb/python/pyoorb.f90",
         ]
         for f in files:
-            if not os.path.exists(os.path.join(
-                'src', os.path.basename(f)
-            )):
-                shutil.copy(f, 'src')
+            if not os.path.exists(os.path.join("src", os.path.basename(f))):
+                shutil.copy(f, "src")
 
-        path = 'pyoorb/data'
-        for f in glob('oorb/data/*.dat'):
-            if not os.path.exists(os.path.join(
-                path, os.path.basename(f)
-            )):
+        path = "pyoorb/data"
+        for f in glob("oorb/data/*.dat"):
+            if not os.path.exists(os.path.join(path, os.path.basename(f))):
                 shutil.copy(f, path)
 
-        shutil.copy('oorb/VERSION', 'pyoorb')
+        shutil.copy("oorb/VERSION", "pyoorb")
 
 
 def version():
     """Generate version using oorb script."""
-    cmd = ['./build-tools/compute-version.sh']
-    return subprocess.check_output(cmd, cwd='oorb').decode().strip()
+    cmd = ["./build-tools/compute-version.sh"]
+    return subprocess.check_output(cmd, cwd="oorb").decode().strip()
 
 
 setup(
     version=version(),
-    cmdclass={
-        'build_src': PyoorbBuildSrc
-    },
-    ext_modules=[NumpyExtension(
-        'pyoorb.ext', ['src/pyoorb.f90'],
-        libraries=['lapack'],
-        extra_objects=['oorb/lib/liboorb.a']
-    )],
+    cmdclass={"build_src": PyoorbBuildSrc},
+    ext_modules=[
+        NumpyExtension(
+            "pyoorb.ext",
+            ["src/pyoorb.f90"],
+            libraries=["lapack"],
+            extra_objects=["oorb/lib/liboorb.a"],
+        )
+    ],
     # install data files
-    package_data={
-        'pyoorb': ['data/*.dat', 'VERSION']
-    }
+    package_data={"pyoorb": ["data/*.dat", "VERSION"]},
 )
